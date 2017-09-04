dcb618e287ecc32d5a319d274b225e586f8d8354
Benjamin Winterberg <winterbe@gmail.com>
2017-09-04 21:20:11 +0200

    Animation

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,42 @@ canvas.height = window.innerHeight
 canvas.width = window.innerWidth
 
 const ctx = canvas.getContext("2d")
-ctx.fillStyle = '#D8D8D8'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-ctx.fillStyle = "#333"
-ctx.font = "32px 'Courier New'"
-ctx.fillText("Hi there! 🚔", 100, 100)
+const car = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    vx: 3,
+    vy: 3,
+    draw() {
+        ctx.save()
+        ctx.fillStyle = "#333"
+        ctx.font = "48px 'Courier New'"
+        ctx.translate(this.x, this.y)
+        ctx.fillText("⚽️", 0, 0)
+        ctx.restore()
+    }
+}
+
+function clear() {
+    ctx.save()
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = '#D8D8D8'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.restore()
+}
+
+function draw() {
+    if (car.x > canvas.width - 48 || car.x < 0) {
+        car.vx = -car.vx
+    }
+    if (car.y > canvas.height || car.y < 48) {
+        car.vy = -car.vy
+    }
+    clear()
+    car.draw()
+    car.x += car.vx
+    car.y += car.vy
+    window.requestAnimationFrame(draw);
+}
+
+draw()
